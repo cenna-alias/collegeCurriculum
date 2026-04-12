8663196e8d48c8e94de6fdbe24d06a915be76760
Cenna Alias <cennaalias29@gmail.com>
2026-04-12 21:11:02 +0530

    emp update

--- S2/Networking/pgm21-employee.sh
+++ S2/Networking/pgm21-employee.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+
 calculate_salary() {
 	emp_no=$1
 	emp_name=$2
 	basic_salary=$3
-	
+
 	if [ $basic_salary -gt 5000 ]; then
 		HRA=$(($basic_salary * 18 / 100))
 	else
 		HRA=550
 	fi
-	
+
 	DA=$((basic_salary * 35 / 100))
 	PF=$((basic_salary * 13 / 100))
 	TA=$((basic_salary * 10 / 100))
-	
+
 	gross_salary=$((basic_salary + HRA + DA + TA))
 	net_salary=$((gross_salary - PF))
-	
+
 	echo "Employee Number: $emp_no"
 	echo "Employee Name: $emp_name"
 	echo "Basic Salary: $basic_salary"
@@ -26,8 +27,8 @@ calculate_salary() {
 	echo "TA: $TA"
 	echo "Gross Salary: $gross_salary"
 	echo "Net Salary: $net_salary"
-	
-	echo "$emp_no, $emp_name, $basic_salary, $HRA, $DA, $PF, $TA, $gross_salary, $net_salary">>Emp_Data.txt
+
+	echo "$emp_no, $emp_name, $basic_salary, $HRA, $DA, $PF, $TA, $gross_salary, $net_salary" >> Emp_Data.txt
 }
 
 read -p "Enter Employee Number: " emp_no
