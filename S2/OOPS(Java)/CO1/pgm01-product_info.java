1295605f80bf0ebd8eb5528b2cfc0cb127bad054
Cenna Alias <cennaalias29@gmail.com>
2026-05-12 09:57:15 +0530

    lab exam

--- S2/OOPS(Java)/CO1/pgm01-product_info.java
+++ S2/OOPS(Java)/CO1/pgm01-product_info.java
@@ -1,53 +1,50 @@
 public class Product
 {
-	int pcode;
-	String pname;
-	double price;
+    int pcode;
+    String pname;
+    double price;
 
-	void data(int c, String n, double p)
-	{
-		pcode = c;
-		pname = n;
-		price = p;
-	}
+    Product(int c, String n, double p)
+    {
+        pcode = c;
+        pname = n;
+        price = p;
+    }
 
-	void display()
-	{
-		System.out.println(pcode + "\t\t" + pname + "\t\t" + price);
-	}
+    void display()
+    {
+        System.out.println(pcode + "\t\t" + pname + "\t\t" + price);
+    }
 
-	static void findLowest(double price1, double price2, double price3)
-	{
-		if(price1 <= price2 && price1 <= price3)
-		{
-			System.out.println("\n Product 1 is of the lowest price!");
-		}
-		else if(price2 <= price1 && price2 <= price3)
-		{
-			System.out.println("\n Product 2 is of the lowest price!");
-		}
-		else
-		{
-			System.out.println("\n Product 3 is of the lowest price!");
-		}
-	}
+    static void findLowest(double price1, double price2, double price3)
+    {
+        if(price1 <= price2 && price1 <= price3)
+        {
+            System.out.println("\n Product 1 is of the lowest price!");
+        }
+        else if(price2 <= price1 && price2 <= price3)
+        {
+            System.out.println("\n Product 2 is of the lowest price!");
+        }
+        else
+        {
+            System.out.println("\n Product 3 is of the lowest price!");
+        }
+    }
 
-	public static void main(String args[])
-	{
-		Product obj1 = new Product();
-		Product obj2 = new Product();
-		Product obj3 = new Product();
+    public static void main(String args[])
+    {
+        Product obj1 = new Product(101, "Product_1", 100.0);
+        Product obj2 = new Product(102, "Product_2", 128.40);
+        Product obj3 = new Product(103, "Product_3", 790.00);
 
-		obj1.data(101, "Product_1", 100.0);
-		obj2.data(102, "Product_2", 128.40);
-		obj3.data(103, "Product_3", 790.00);
+        System.out.println("Product Information:");
+        System.out.println("Pcode\t\tPname\t\tPrice");
+        System.out.println("--------------------------------------------------");
+        obj1.display();
+        obj2.display();
+        obj3.display();
 
-		System.out.println("Product Information:\nProduct_code\tProduct_name\tProduct_price");
-
-		obj1.display();
-		obj2.display();
-		obj3.display();
-
-		findLowest(obj1.price, obj2.price, obj3.price);
-	}
+        findLowest(obj1.price, obj2.price, obj3.price);
+    }
 }
